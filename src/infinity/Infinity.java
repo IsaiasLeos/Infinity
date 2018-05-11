df1cf480014247437dc478f2074ff06d5e94999b
Pantherinae <blade_master_pantherinae@hotmail.com>
2018-05-11 14:24:24 -0600

    Test Commit

--- src/infinity/Infinity.java
+++ src/infinity/Infinity.java
@@ -1,23 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package infinity;
 
 /**
  *
- * @author Isaias Leos Ayala
+ * @author Ivan Mota, Isaias Leos Ayala
  */
 public class Infinity
 {
 
-	/**
-	 * @param args the command line arguments
-	 */
 	public static void main(String[] args)
 	{
-		// TODO code application logic here
 	}
 	
 }
