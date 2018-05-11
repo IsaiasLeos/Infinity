a934cc59036b8e11cc85d0ad2a8d33fc989c69d0
Ivan <ivanmtta@gmail.com>
2018-05-11 16:23:54 -0600

    collitions

--- src/InfinityTest.java
+++ src/InfinityTest.java
@@ -1,6 +1,7 @@
 import java.util.LinkedList;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
 
 public class InfinityTest
 {
@@ -11,12 +12,13 @@ public class InfinityTest
 	public void onCreate()
 	{
 		for(int i = 0; i < 6; i++){
-			platforms.add(new Platform(i * 200, 400, 150, 100));
+			platforms.add(new Platform(i * 220, 400, 150, 100));
 		}
 	}
 
 	public void update()
 	{
+		checkCollitions();
 		person.update();
 		for(int i = 0; i < platforms.size(); i++){
 			platforms.get(i).x -= 1;
@@ -39,6 +41,15 @@ public class InfinityTest
 	{
 		System.out.println("player.Jump()");
 		person.jumping = true;
+	}
+
+	public void checkCollitions(){
+		for(int i = 0; i < platforms.size(); i++){
+			if(platforms.get(i).getBounds().contains(new Point2D.Double(person.getX(), person.getY() + person.getSize() + 1)) ||
+				platforms.get(i).getBounds().contains(new Point2D.Double(person.getX() + person.getSize(), person.getY() + person.getSize() + 1))){
+				person.falling = false;
+			}
+		}
 	}
 
 }
--- src/Platform.java
+++ src/Platform.java
@@ -19,4 +19,8 @@ public class Platform{
 		Rectangle2D box = new Rectangle2D.Double(x, y, width, height);
 		g.fill(box);
 	}
+
+	public Rectangle2D getBounds(){
+		return new Rectangle2D.Double(x, y, width, height);
+	}
 }
