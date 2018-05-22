556a4a356cd7b37a7a2235d7010ec7c3d22b3896
Ivan <ivanmtta@gmail.com>
2018-05-22 16:56:52 -0600

    Finished Platform generation system (Now the game its playable)

--- src/PlatformGenerator.java
+++ src/PlatformGenerator.java
@@ -4,34 +4,22 @@ import java.util.Random;
 
 public class PlatformGenerator {
 
-	public LinkedList<Platform> plat = new LinkedList<>();
 	public int x;
-	public int y;
 
-	public PlatformGenerator(int x, int y) {
+	public Platform generatePlatform(int x){
 		Random rng = new Random();
 		int platformNum = rng.nextInt(5);
 		switch(platformNum) {
 			case 0:
-				Platform p1 = new Platform(x, y, 150, 100);
-				plat.add(p1);
-				break;
+				return new Platform(x, 400, 300, 400);
 			case 1:
-				Platform p2 = new Platform(x, y, 100, 100);
-				plat.add(p2);
-				break;
+				return new Platform(x, 450, 400, 400);
 			case 2:
-				Platform p3 = new Platform(x, y, 300, 100);
-				plat.add(p3);
-				break;
+				return new Platform(x, 500, 450, 400);
 			case 3:
-				Platform p4 = new Platform(x, y, 200, 100);
-				plat.add(p4);
-				break;
+				return new Platform(x, 400, 500, 400);
 			default:
-				Platform p5 = new Platform(x, y, 250, 100);
-				plat.add(p5);
-				break;
+				return new Platform(x, 450, 300, 400);
 		}
 	}
 }
