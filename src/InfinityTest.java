d8e1931332f44165bded5fad39bc61f1a017b4d2
Pantherinae <blade_master_pantherinae@hotmail.com>
2018-05-23 20:36:03 -0500

    Removed Potion.java implementation from InfinityTest due to re-work

--- src/InfinityTest.java
+++ src/InfinityTest.java
@@ -42,22 +42,15 @@ public class InfinityTest {
 		else if(scoreboard.score > scoreboard.highScore) {
 			scoreboard.writeHighScore();
 		}
-
-//		checkPotionCollision();
-//		potion.update();
 		//Checks whether the anything is colliding with the ride side of the platform.
 		//Stops movement of platforms if anything is colliding.
 		for(int i = 0; i < platforms.size(); i++) {
 			if(!player.collidingRight) {
 				platforms.get(i).x -= platformSpeed;
-//				potion.movement = 2;
 				if(debug) {
 					rectCoord = platforms.get(i).x;
 				}
 			}
-//			else {
-//				potion.movement = 0;
-//			}
 		}
 	}
 
@@ -66,7 +59,6 @@ public class InfinityTest {
 			isDebugEnabled(g);
 		}
 		player.draw(g);
-//		potion.draw(g);
 		for(int i = 0; i < platforms.size(); i++) {
 			platforms.get(i).draw(g);
 		}
@@ -95,10 +87,6 @@ public class InfinityTest {
 		g.drawString("Y: " + Integer.toString(player.y), 1150, 175);
 		g.drawString("isPlayerDead: " + Boolean.toString(player.isPlayerDead()), 1150, 200);
 		g.drawString("rectangle Coord: " + Integer.toString(rectCoord), 1150, 225);
-//		g.drawString("PotionX: " + Integer.toString(potion.x), 1150, 250);
-//		g.drawString("PotionY: " + Integer.toString(potion.y), 1150, 275);
-//		g.drawString("itemsCollided: " + Boolean.toString(potion.itemCollided), 1150, 300);
-
 	}
 
 	/**
@@ -141,54 +129,6 @@ public class InfinityTest {
 //		System.out.println(event);
 	}
 
-	/**
-	 * Checks the items collisions. TODO: Fix indentation
-	 */
-//	public void checkPotionCollision() {
-//		if((player.x - (player.size / 2)//Todo - Improve Logic
-//			== potion.x || player.x + (player.size / 2)
-//			== potion.x) && player.y == potion.y) {
-//			potion.itemCollided = true;
-//		}
-//		potion.collidingTop = false;
-//		potion.collidingBot = false;
-//		for(int i = 0; i < platforms.size(); i++) {
-//			int platformX = platforms.get(i).x;
-//			int platformY = platforms.get(i).y;
-//			int platformW = platforms.get(i).width;
-//			int platformH = platforms.get(i).height;
-//			if(potion.y <= platformY + platformH
-//				&& potion.y + potion.size >= platformY
-//				&& potion.x <= platformX + platformW
-//				&& potion.x + potion.size >= platformX) {
-//				if(potion.x <= platformX + platformW - 2
-//					&& potion.x + potion.size - 2 >= platformX
-//					&& potion.y > platformY) {
-//					potion.collidingTop = false;
-//					potion.jumping = false;
-//					potion.falling = true;
-//					potion.jumpSpd = potion.jumpOriginalVal;
-//				}
-//				if(potion.x <= platformX + platformW - 2
-//					&& potion.x + potion.size - 2 >= platformX
-//					&& potion.y + potion.size < platformY + platformH) {
-//					potion.y = platformY - potion.size;
-//					potion.collidingBot = true;
-//					potion.falling = false;
-//					potion.fallSpd = 0;
-//				}
-//				if(potion.x + potion.size == platformX && potion.y + potion.size != platformY) {
-//					potion.collidingRight = true;
-//				}
-//				if(potion.x == platformX + platformW) {
-//					potion.collidingLeft = true;
-//				}
-//			}
-//			if(!potion.collidingBot) {
-//				potion.falling = true;
-//			}
-//		}
-//	}
 	/**
 	 * Checks the player's collisions. TODO: Fix indentation
 	 */
